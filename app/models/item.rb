d86ec9f17ffa0641ce0536f976400a70055e76a3
Gasai-005 <82623062+Gasai-005@users.noreply.github.com>
2021-05-25 18:32:13 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   
   with_options presence: true do
     validates :name
@@ -8,8 +9,17 @@ class Item < ApplicationRecord
     validates :delivery_cost_id
     validates :delivery_day_id
     validates :prefecture_id
-    validates :user
     validates :category_id
     validates :status_id
   end
+
+  with_options numericality: { other_than: 1 } do
+    validates :delivery_cost_id
+    validates :delivery_day_id
+    validates :prefecture_id
+    validates :category_id
+    validates :status_id
+  end
+
+  validates_inclusion_of :price, in:300..9999999, message: 'を￥300~9999999の範囲で設定してください'
 end
