7ced8995932098d9b701ec1bb7ab3a9b0810f2ec
Gasai-005 <82623062+Gasai-005@users.noreply.github.com>
2021-05-27 20:42:17 +0900

    商品出品機能の修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
     name             {Faker::Name.initials(number: 2)}
     description      {Faker::Lorem.paragraph(sentence_count: 2)}
     price            {Faker::Number.number(digits: 4)}
-    delivery_cost_id { "2" }
-    delivery_day_id  { "2" }
-    prefecture_id    { "2" }
-    category_id      { "2" }
-    status_id        { "2" }
+    delivery_cost_id { 2 }
+    delivery_day_id  { 2 }
+    prefecture_id    { 2 }
+    category_id      { 2 }
+    status_id        { 2 }
     association :user
 
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,27 +29,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it "categpryのidが1だと出品できない" do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it "statusのidが1だと出品できない" do
-        @item.status_id = '1'
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it "delivery_costのidが1だと出品できない" do
-        @item.delivery_cost_id = '1'
+        @item.delivery_cost_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery cost must be other than 1")
       end
       it "delivery_dayのidが1だと出品できない" do
-        @item.delivery_day_id = '1'
+        @item.delivery_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day must be other than 1")
       end
       it "prefectureのidが1だと出品できない" do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
@@ -59,17 +59,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "priceが￥300より少ないと出品できない" do
-        @item.price = "1"
+        @item.price = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Price を半角数字で￥300~9999999の範囲に設定してください")
       end
       it "priceが￥9999999より大きいと出品できない" do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price を半角数字で￥300~9999999の範囲に設定してください")
       end
       it "priceが半角数字のみでないと出品できない" do
         @item.price = '２０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price を半角数字で￥300~9999999の範囲に設定してください")
+      end
+      it "priceが半角英語のみでは出品できない" do
+        @item.price = "aaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price を半角数字で￥300~9999999の範囲に設定してください")
+      end
+      it "priceが半角英数混合では出品できない" do
+        @item.price = "a300"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price を半角数字で￥300~9999999の範囲に設定してください")
       end
